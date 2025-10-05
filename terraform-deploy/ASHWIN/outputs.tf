569bf3d00a939434a095a70877b3961a565b08be
Ashwin <116995118+heel-Ashwin@users.noreply.github.com>
2025-10-05 22:39:34 +0530

    Delete terraform-deploy/ASHWIN directory

--- terraform-deploy/ASHWIN/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip" {
-  value = aws_instance.usp_ec2.public_ip
-}
--- terraform-deploy/ASHWIN/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "key_name" {
-  description = "AWS key pair name"
-  type        = string
-}
-
-variable "instance_type" {
-  default     = "t3.micro"
-}
