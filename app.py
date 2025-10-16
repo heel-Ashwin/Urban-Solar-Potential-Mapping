32c2ed093dc5cb82bb5b4bb2e21e27fe77d5f69d
Ashwin <116995118+heel-Ashwin@users.noreply.github.com>
2025-10-16 08:18:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import numpy as np
@@ -127,4 +128,4 @@ def predict():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
